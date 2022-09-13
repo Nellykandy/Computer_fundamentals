d0ecffe228c09d71ec4163387d10e38898a618bc
Nellykandy <105287961+Nellykandy@users.noreply.github.com>
2022-09-13 12:19:35 +0100

    Add main menu

--- main.py
+++ main.py
@@ -82,29 +82,29 @@ def option4():
     plt.title('the top songs')
     plt.xlabel('in percent')
     plt.xlabel('in percent')
-#     plt.show()
-# if __name__=='__main__':
-#     while(True):
-#         print_menu()
-#         option = ''
-#         try:
-#             option = int(input('Enter your choice: '))
-#         except:
-#             print('Wrong input. Please enter a number ...')
-#         #Check what choice was entered and act accordingly
-#         if option == 1:
-#            option1()
-#         elif option == 2:
-#             option2()
-#         elif option == 3:
-#             option3()
-#         elif option == 4:
-#             option4()
-#         elif option == 5:
-#             print('Thanks message before exiting')
-#             exit()
-#         else:
-#             print('Invalid option. Please enter a number between 1 and 4.')
+    plt.show()
+if __name__=='__main__':
+    while(True):
+        print_menu()
+        option = ''
+        try:
+            option = int(input('Enter your choice: '))
+        except:
+            print('Wrong input. Please enter a number ...')
+        #Check what choice was entered and act accordingly
+        if option == 1:
+           option1()
+        elif option == 2:
+            option2()
+        elif option == 3:
+            option3()
+        elif option == 4:
+            option4()
+        elif option == 5:
+            print('Thanks message before exiting')
+            exit()
+        else:
+            print('Invalid option. Please enter a number between 1 and 4.')
 
 	
 
